fdc550a4e49b095bcaa8f0a5aa664727ca7048b6
Diamond Acharya <diamondacharya139@gmail.com>
2025-10-02 23:36:15 -0500

    deploy the site with github pages on diamondacharya.github.io/portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio/', // makes sure assets are correctly linked when deployed to GitHub Pages
   plugins: [react()],
 })
