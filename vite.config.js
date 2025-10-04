d886033fff7523ddcc342fb5b53ee571a453a09c
Diamond Acharya <diamondacharya139@gmail.com>
2025-10-03 23:30:43 -0500

    change homepage in package.json to custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/', // makes sure assets are correctly linked when deployed to GitHub Pages
+  base: '/', // makes sure asset paths are correctly resolved
   plugins: [react()],
 })
