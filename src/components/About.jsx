596f22d940ebbb7c5248800c208e63f23189c8df
Diamond Acharya <diamondacharya139@gmail.com>
2026-05-07 09:59:27 -0500

    change 'hoping' to 'planning'

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
         &lt;&lt; A lil about me &gt;&gt;
       </div>
       <p style={{ lineHeight: 1.8, color: '#666' }}>
-        A software engineer by profession, I have a passion for solving complex problems and building scalable solutions that add value to people's lives. I currently work for Pizza Hut and am pursuing a Master's degree in Computer Science that I'm hoping to complete by the end of this year.
+        A software engineer by profession, I have a passion for solving complex problems and building scalable solutions that add value to people's lives. I currently work for Pizza Hut and am pursuing a Master's degree in Computer Science that I'm planning to complete by the end of this year.
       </p>
     </div>
   )
